d8d706586be7d61ebb9e37b724aba6e65c8defa1
Vutsal Singhal <vutsalsinghal@gmail.com>
2019-04-07 17:54:22 -0400

    updated build files

---
+++ precache-manifest.b2fae0459ef55f758cb3e616a5f18495.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "28229984c437847172d2",
+    "url": "/react-google-maps-search/static/js/runtime~main.7ecf8769.js"
+  },
+  {
+    "revision": "55d6111b531d403e43a8",
+    "url": "/react-google-maps-search/static/js/main.ebaa6d8d.chunk.js"
+  },
+  {
+    "revision": "0d3fc2734485c8bfd92c",
+    "url": "/react-google-maps-search/static/js/2.71fe322d.chunk.js"
+  },
+  {
+    "revision": "55d6111b531d403e43a8",
+    "url": "/react-google-maps-search/static/css/main.667f00dc.chunk.css"
+  },
+  {
+    "revision": "2e4859280be728c289374f8592e967b1",
+    "url": "/react-google-maps-search/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8b646fb1a678110c9533836ba8ac5541.js"
+  "/react-google-maps-search/precache-manifest.b2fae0459ef55f758cb3e616a5f18495.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-google-maps-search/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
