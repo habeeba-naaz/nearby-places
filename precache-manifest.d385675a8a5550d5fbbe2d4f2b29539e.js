aa042fb8fc8fe6a4737dbd08a18fb06efec46571
vutsalsinghal <vutsalsinghal@gmail.com>
2019-04-28 14:09:55 +0000

    [skip ci] Updated build files

---
+++ precache-manifest.d385675a8a5550d5fbbe2d4f2b29539e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "804cf6e6f0c4817bf361",
+    "url": "/nearby-places/static/js/runtime~main.6ec1b2ca.js"
+  },
+  {
+    "revision": "2ff3f6d73a3a334de614",
+    "url": "/nearby-places/static/js/main.1927925e.chunk.js"
+  },
+  {
+    "revision": "5537a0ed5f40d91209a0",
+    "url": "/nearby-places/static/js/2.9d354e31.chunk.js"
+  },
+  {
+    "revision": "2ff3f6d73a3a334de614",
+    "url": "/nearby-places/static/css/main.1912b301.chunk.css"
+  },
+  {
+    "revision": "d3586b35cac6cfd2c516062fff2c6e97",
+    "url": "/nearby-places/index.html"
+  }
+];
